bd1599a13eede59730fa323ac508a9d90a4b8f6d
YassineKtr <yassinektiri@gmail.com>
2024-08-25 00:40:43 +0100

    Testing

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,8 @@ export default function Home() {
   return (
     <>
       <main className="">
-        {/* <HomePage />
-        <BackToTop /> */}
+        <HomePage />
+        <BackToTop />
       </main>
     </>
   );
