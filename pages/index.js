8f616cc72094ca43fac5e61bb269191424b907bd
Yassine Essebbar <essebbar.yassine@gmail.com>
2024-08-25 15:52:17 +0100

    chore: refacto homepage components

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,11 @@
-import HomePage from "./01-main-demo";
 import BackToTop from "./backToTop";
+import HomePage from "./08-language-academy";
 
 export default function Home() {
   return (
     <>
       <main className="">
         <HomePage />
-        <BackToTop />
       </main>
     </>
   );
