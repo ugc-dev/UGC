5ca3a2eca0b07721519ad9f1a23e66bfd9633849
Yassine Essebbar <essebbar.yassine@gmail.com>
2024-11-18 18:58:31 +0100

    Refactor: remove unused components

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,32 @@
-import HomePage from "./UGC/Home";
+import PageHead from "@/pages/Head";
+import { Provider } from "react-redux";
+import Store from "@/redux/store";
+import BackToTop from "@/pages/backToTop";
 
-export default function Home() {
+import Context from "@/context/Context";
+
+import HomePageContent from "@/components/HomePageContent";
+import HeaderStyleNine from "@/components/Header/HeaderStyle-Nine";
+import MobileMenu from "@/components/Header/MobileMenu";
+import Separator from "@/components/Common/Separator";
+import FooterOne from "@/components/Footer/Footer-One";
+
+const Home = () => {
   return (
     <>
-      <main className="">
-        <HomePage />
-      </main>
+      <PageHead title="Home Classic Lms - Online Courses & Education NEXTJS14 Template" />
+      <Provider store={Store}>
+        <Context>
+          <MobileMenu />
+          <HeaderStyleNine headerType="" />
+          <HomePageContent />
+          <BackToTop />
+          <Separator />
+          <FooterOne />
+        </Context>
+      </Provider>
     </>
   );
-}
+};
+
+export default Home;
